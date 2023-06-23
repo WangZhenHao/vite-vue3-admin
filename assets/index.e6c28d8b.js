3fe81206cd4342147d5437566bf2ed7595b00b6c
WangZhenHao <1471047476@qq.com>
2023-06-23 07:18:43 +0000

    Deploying to gh-pages from @ WangZhenHao/vite-vue3-admin@e0650155217ed5419d7d5ede6ccdbfa70eb3ed8f 🚀

---
+++ assets/index.e6c28d8b.js
@@ -0,0 +1,1 @@
+import{d as t,o,c,g as a}from"./index.8ad4f63f.js";const i=t({__name:"index",setup(s){function n(){a().then(e=>{console.log(e)})}return n(),(e,r)=>(o(),c("div",null,"\u554A\u6C34\u6C34"))}});export{i as default};
